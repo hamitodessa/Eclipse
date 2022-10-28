942b306e90ed19b3b4cdc78f80c2d79b062e4d64
hamit okumus <hamit@okumus.com>
2022-10-29 00:42:50 +0300

    1

--- OBS_CLASS_2025/LOGER_KAYIT/LOGER_KAYIT/DOSYA_MSSQL.java
+++ OBS_CLASS_2025/LOGER_KAYIT/LOGER_KAYIT/DOSYA_MSSQL.java
@@ -11,21 +11,26 @@ import OBS_C_2025.GLOBAL;
 public class DOSYA_MSSQL implements ILOGER_KAYIT{
 	
 	 Connection con = null;
-	String lOG_Conn_L = "localhost;instanceName=" + BAGLAN.cariDizin.iNSTANCE + " ; database=" + "OK_Car" +  BAGLAN.cariDizin.kOD + "_LOG" ;
-	String lOG_Conn_S = BAGLAN.cariDizin.sERVER + ";instanceName=" + BAGLAN.cariDizin.iNSTANCE + " ; database=" + "OK_Car" +  BAGLAN.cariDizin.kOD + "_LOG" ;
 	
 	public void Logla(String mesaj, String evrak) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-			
-				
+		BAGLAN bAGLAN = new BAGLAN();	
+		bAGLAN.Connect(GLOBAL.KULL_ADI);
+		String lOG_Conn_L = "localhost;instanceName=" + BAGLAN.cariDizin.iNSTANCE + " ; database=" + "OK_Car" +  BAGLAN.cariDizin.kOD + "_LOG" ;
+		//String lOG_Conn_S = BAGLAN.cariDizin.sERVER + ";instanceName=" + BAGLAN.cariDizin.iNSTANCE + " ; database=" + "OK_Car" +  BAGLAN.cariDizin.kOD + "_LOG" ;
+		
+		System.out.println("cariconn=" + BAGLAN.cariDizin.cONN_STR);
+		System.out.println("conn=" +lOG_Conn_L);
 		String cumle = "jdbc:sqlserver://" + lOG_Conn_L + ";";
 	    con = DriverManager.getConnection(cumle,BAGLAN.cariDizin.kULLANICI,BAGLAN.cariDizin.sIFRESI);
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String sql  = "INSERT LOGLAMA (TARIH,EVRAK,MESAJ,[USER]) " +
-    		   		  " VALUES (?,?,?,?,?)" ;
+        String sql  = "INSERT LOGLAMA (TARIH,EVRAK,MESAJ,[USER_NAME]) " +
+    		   		  " VALUES (?,?,?,?)" ;
     	PreparedStatement stmt = null;
     	stmt = con.prepareStatement(sql);
-		stmt.setDate(2, java.sql.Date.valueOf(java.time.LocalDate.now()));
+    	java.util.Date today = new java.util.Date();
+        
+		stmt.setTimestamp(1, new java.sql.Timestamp(today.getTime()));
 		stmt.setString(2, evrak);
 		stmt.setString(3, mesaj);
 		stmt.setString(4, GLOBAL.KULL_ADI);

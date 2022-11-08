f89ab259bab961481047bbe8e920515d6682be0a
Hamit <hamit@okumus.com>
2022-11-08 23:16:20 +0300

    1

--- OBS_SISTEM_2025/GRAFIK_GORUNUM/OBS_2025/LineChartEx.java
+++ OBS_SISTEM_2025/GRAFIK_GORUNUM/OBS_2025/LineChartEx.java
@@ -7,13 +7,13 @@ import java.awt.Font;
 import javax.swing.BorderFactory;
 import javax.swing.JInternalFrame;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
+
+import org.jfree.chart.*;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.chart.title.TextTitle;
+import org.jfree.data.*;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;

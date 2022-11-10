cdb194076ba46593c19274b6fe2ef1caee274c2d
Hamit <hamit@okumus.com>
2022-11-10 23:05:01 +0300

    1

--- OBS_SISTEM_2025/START/OBS_2025/CAL_DIZIN.java
+++ OBS_SISTEM_2025/START/OBS_2025/CAL_DIZIN.java
@@ -355,25 +355,27 @@ public class CAL_DIZIN extends JFrame {
 							txtsif.requestFocus();
 			                return;
 						}
-		            contentPane.setCursor(WAIT_CURSOR);
-		            oac.uSER_ISL.sifre_degis(GLOBAL.KULL_ADI, txtyenisif.getText());
-		            txtyenisif.setText("");
-		            txtsif.setText("");
-		            txtyenisif.setVisible(false);
-		            lblysif.setVisible(false); 
-		            txtsif.requestFocus();
+						contentPane.setCursor(WAIT_CURSOR);
+						oac.uSER_ISL.sifre_degis(GLOBAL.KULL_ADI, txtyenisif.getText());
+						txtyenisif.setText("");
+						txtsif.setText("");
+						txtyenisif.setVisible(false);
+						lblysif.setVisible(false); 
+						txtsif.requestFocus();
 		      
 	            	}
 	            	else if (activ_sayfa == 8)
 	            	{
 	            		String oku = txt_Lmaill .getText();
-						if (oku.toString() != "")
+						if (! oku.equals(""))
 						{
-							
-						     ResultSet	rs = null;
+						    JOptionPane.showMessageDialog(null,  "1", "Dosya Olusturma", JOptionPane.PLAIN_MESSAGE);
+						    ResultSet	rs = null;
 							oac.uSER_ISL.log_mail_kont_kayit(GLOBAL.KULL_ADI , oku);
 							rs = oac.uSER_ISL.log_mail_oku(GLOBAL.KULL_ADI);
 							if (!rs.isBeforeFirst() ) {  
+								   JOptionPane.showMessageDialog(null,  "2", "Dosya Olusturma", JOptionPane.PLAIN_MESSAGE);
+
 							    return;
 							} 
 							else
@@ -381,8 +383,9 @@ public class CAL_DIZIN extends JFrame {
 								cmb_maillist.removeAllItems();
 								 while (rs.next()) 
 								 {
+									    JOptionPane.showMessageDialog(null,  "3", "Dosya Olusturma", JOptionPane.PLAIN_MESSAGE);
 									 cmb_maillist.addItem(rs.getString("E_MAIL"));
-							        }
+							     }
 								
 							}
 							 txt_Lmaill.setText(oac.uSER_ISL.log_mail_aktiv_oku(GLOBAL.KULL_ADI));
@@ -393,8 +396,11 @@ public class CAL_DIZIN extends JFrame {
 	                contentPane.setCursor(DEFAULT_CURSOR);
 	            }
 	            catch (Exception ex)
+			  	
 	            {
 	            contentPane.setCursor(DEFAULT_CURSOR);
+	            JOptionPane.showMessageDialog(null,  ex.getMessage(), "Mail Kaydet", JOptionPane.PLAIN_MESSAGE);
+	  	      
 	            txtsif.requestFocus();		
 }				}
 			

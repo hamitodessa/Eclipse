6eaf61495e2bc817661e6c367c799d8570cee8a8
Hamit <hamit@okumus.com>
2022-11-20 12:41:47 +0200

    1

--- OBS_SISTEM_2025/PDF_GOSTER/OBS_2025/PDF_GOSTERR.java
+++ OBS_SISTEM_2025/PDF_GOSTER/OBS_2025/PDF_GOSTERR.java
@@ -3,12 +3,23 @@ package OBS_2025;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
-
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import org.icepdf.ri.common.ComponentKeyBinding;
+import org.icepdf.ri.common.SwingController;
+import org.icepdf.ri.common.SwingViewBuilder;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+
 import java.awt.BorderLayout;
 
 public class PDF_GOSTERR extends JInternalFrame {
 
+	public JPanel panel = new JPanel();
+	 public JScrollPane jscpn = new  JScrollPane();
 	/**
 	 * Launch the application.
 	 */
@@ -36,12 +47,34 @@ public class PDF_GOSTERR extends JInternalFrame {
 		setResizable(true);
 		setBounds(0, 0, 1000, 600);
 		
-		JPanel panel = new JPanel();
-		getContentPane().add(panel, BorderLayout.CENTER);
+		
+	 jscpn = new  JScrollPane();
+		 
+		 panel = new JPanel();
+		 jscpn.add(panel);
+		getContentPane().add(jscpn, BorderLayout.CENTER);
 		// System.out.println("fdsfdsfsd");
 		
-	
+		pdf_goster();
       
 	}
 
+	public void pdf_goster()
+	{
+		 try {
+			 String file = "C:\\qwerty.pdf" ;
+	           SwingController control=new SwingController();
+	            SwingViewBuilder factry=new SwingViewBuilder(control);
+	            panel =factry.buildViewerPanel();
+	            ComponentKeyBinding.install(control, panel);
+	      
+	            control.getDocumentViewController().setAnnotationCallback(
+	                    new org.icepdf.ri.common.MyAnnotationCallback(
+	                    control.getDocumentViewController()));
+	                   control.openDocument(file);
+	     jscpn.setViewportView(panel); 
+	        } catch (Exception ex) {
+	            JOptionPane.showMessageDialog(this,"Cannot Load Pdf");
+	        }
+	}
 }

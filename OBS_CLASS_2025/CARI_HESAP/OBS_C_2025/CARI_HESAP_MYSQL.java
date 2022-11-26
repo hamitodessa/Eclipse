d35608ae687bbb00ce8ee9133fe8e25ab8677e30
Hamit <hamit@okumus.com>
2022-11-26 23:06:07 +0200

    1

--- OBS_CLASS_2025/CARI_HESAP/OBS_C_2025/CARI_HESAP_MYSQL.java
+++ OBS_CLASS_2025/CARI_HESAP/OBS_C_2025/CARI_HESAP_MYSQL.java
@@ -1010,6 +1010,8 @@ public class CARI_HESAP_MYSQL implements ICARI_HESAP {
         		"        		AND h.HESAP_CINSI BETWEEN N'"+ c1 + "' AND '"+ c2 +"'" + 
         		"              AND h.KARTON BETWEEN N'"+ k1 + "' AND N'" + k2 + "'" + 
         		"              GROUP BY h.HESAP, h.UNVAN, h.HESAP_CINSI" + o1 + " " + o2 + "" ;
+        
+ 
       	PreparedStatement stmt = con.prepareStatement(sql);
 		rss = stmt.executeQuery();
 		return rss;	

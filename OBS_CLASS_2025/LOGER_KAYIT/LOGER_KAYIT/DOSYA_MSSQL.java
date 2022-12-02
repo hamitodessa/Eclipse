4e7b8a3087f7ec632604249a51de779ec7c0fd63
hamit <hamit@192.168.0.94>
2022-12-02 11:24:49 +0200

    1

--- OBS_CLASS_2025/LOGER_KAYIT/LOGER_KAYIT/DOSYA_MSSQL.java
+++ OBS_CLASS_2025/LOGER_KAYIT/LOGER_KAYIT/DOSYA_MSSQL.java
@@ -6,8 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.swing.JOptionPane;
-
 import OBS_C_2025.DIZIN_BILGILERI;
 import OBS_C_2025.GLOBAL;
 
@@ -39,12 +37,38 @@ public class DOSYA_MSSQL implements ILOGER_KAYIT{
 		
 	}
 
-	
-	public ResultSet log_rapor(String t1, String t2, String aciklama, String evrak ,String user, DIZIN_BILGILERI dBILGI)
+	@Override
+	public ResultSet log_rapor(String t1, String t2, String aciklama, String evrak, String user, DIZIN_BILGILERI dBILGI)
 			throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		  
+		 StringBuilder stb = new StringBuilder();
+        stb.append(" SELECT DATE_FORMAT(TARIH, '%d.%m.%Y %H:%i:%s') AS TARIH ,MESAJ,EVRAK,USER_NAME " ); 
+        stb.append(" FROM   loglama  USE INDEX (IX_LOGLAMA) ") ; 
+      
+        stb.append(" WHERE  loglama.mesaj  LIKE N'" + aciklama + "'") ;
+        if ( ! t1.equals(""))
+        		{
+            stb.append(" AND TARIH BETWEEN  '" + t1 + "' AND '" + t2 + " 23:59:59.998' ") ;
+        		}
+        if ( ! evrak.equals("%") &&  ! evrak.equals("%%")) 
+        {
+            stb.append(" AND EVRAK  LIKE '" + user + "'");
+        }
+       if ( ! user.equals("%") &&  ! user.equals("%%")) 
+        {
+            stb.append(" AND USER_NAME  LIKE '" + user + "'");
+        }
+        stb.append(" ORDER BY TARIH ") ;
+        String cumle = "jdbc:sqlserver://" + dBILGI.cONN_STR + ";";
+	    con = DriverManager.getConnection(cumle,dBILGI.kULLANICI,dBILGI.sIFRESI);
+		ResultSet	rss = null;
+        String sql = stb.toString() ;
+    	PreparedStatement stmt = con.prepareStatement(sql);
+		rss = stmt.executeQuery();
+		return rss;	
 	}
+
+	
 
 	
 
--- OBS_CLASS_2025/LOGER_KAYIT/LOGER_KAYIT/ILOGER_KAYIT.java
+++ OBS_CLASS_2025/LOGER_KAYIT/LOGER_KAYIT/ILOGER_KAYIT.java
@@ -10,5 +10,5 @@ import OBS_C_2025.DIZIN_BILGILERI;
 public interface ILOGER_KAYIT {
 	
 	public void Logla(String mesaj, String evrak, DIZIN_BILGILERI dBILGI) throws ClassNotFoundException, SQLException ;
-	
+	public ResultSet log_rapor(String t1, String t2, String aciklama, String evrak , String user, DIZIN_BILGILERI dBILGI) throws ClassNotFoundException, SQLException;
 }

b2c23388a0a6c20dbfb41881120aa7cffbe1698c
Hamit <hamit@okumus.com>
2023-01-10 13:13:03 +0300

    1

--- VERITABANI_YEDEK/YARDIMCI_DOSYALAR/VT_YEDEK/GOREV.java
+++ VERITABANI_YEDEK/YARDIMCI_DOSYALAR/VT_YEDEK/GOREV.java
@@ -8,17 +8,12 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
-import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
@@ -42,102 +37,102 @@ public class GOREV {
 	static ResultSet rss  ;
 	static boolean varmi = false;
 	Date    sonrakitar = new Date();
-	static int gunSAYISI= 6 ;
+	static int hangiGUNDEYIZ ;
 	public static  JPanel getShowRoomPanel(String emirAdi ,String sonDurum,int dosyaSayisi,String sonYEDEK , String gelYEDEK,
 			String acikLAMA,String durUM,String surUCU) throws InterruptedException, NumberFormatException, ClassNotFoundException, SQLException, ParseException
 	{
-			durdur();
-	        p = new JPanel(new GridBagLayout());
-	        p.setBorder(new TitledBorder(emirAdi));
-	        ((javax.swing.border.TitledBorder) p.getBorder()).setTitleFont(new Font("Arial", Font.BOLD, 14));
-	         ((TitledBorder) p.getBorder()).setTitleColor(Color.BLUE);
-	  //  jPanel1.repaint();
-	        GridBagConstraints gbc = new GridBagConstraints();
-	        gbc.insets = new Insets(5, 5, 5, 5);
-	        gbc.anchor = GridBagConstraints.WEST;
+		durdur();
+		p = new JPanel(new GridBagLayout());
+		p.setBorder(new TitledBorder(emirAdi));
+		((javax.swing.border.TitledBorder) p.getBorder()).setTitleFont(new Font("Arial", Font.BOLD, 14));
+		((TitledBorder) p.getBorder()).setTitleColor(Color.BLUE);
+		//  jPanel1.repaint();
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbc.anchor = GridBagConstraints.WEST;
 
-	        p.add(new JLabel("Son Durum:" ), gbc);
-	        gbc.gridx = 1 ;
-	        p.add(new JLabel(sonDurum), gbc);
-	
-	        gbc.gridx = 2;
-	        p.add(new JLabel("          Son Yedekleme :" ) , gbc);
-	        
-	        gbc.gridx = 3;
-	        lblson = new JLabel(sonYEDEK );
-	        lblson.setForeground(Color.green.darker());
-	        p.add(lblson, gbc);
+		p.add(new JLabel("Son Durum:" ), gbc);
+		gbc.gridx = 1 ;
+		p.add(new JLabel(sonDurum), gbc);
 
-	        gbc.gridy = 1;
-	        gbc.gridx = 0;
-	        p.add(new JLabel("Dosya Sayisi   :"), gbc);
+		gbc.gridx = 2;
+		p.add(new JLabel("          Son Yedekleme :" ) , gbc);
 
-	        gbc.gridx = 1;
-	        p.add(new JLabel(Integer.toString(dosyaSayisi) + " Adet Dosya" ), gbc);
+		gbc.gridx = 3;
+		lblson = new JLabel(sonYEDEK );
+		lblson.setForeground(Color.green.darker());
+		p.add(lblson, gbc);
 
-	        gbc.gridx = 2;
-	        p.add(new JLabel("          Gelecek Yedekleme :" ), gbc);
-	        gbc.gridx = 3;
-	        
-	        lblgel = new JLabel(gelYEDEK );
-	        lblgel.setForeground(Color.RED);
-	        p.add(lblgel, gbc);
+		gbc.gridy = 1;
+		gbc.gridx = 0;
+		p.add(new JLabel("Dosya Sayisi   :"), gbc);
 
-	        gbc.gridy = 2;
-	        gbc.gridx = 0;
-	        gbc.weightx = 0.0;
-	        gbc.gridwidth = 2;
-	        p.add(new JLabel("Surucu / FTP  :" ), gbc);
-	        
-	        gbc.gridx = 1;
-	        p.add(new JLabel(surUCU), gbc);
-	        
-	        gbc.gridy = 3;
-	        gbc.gridx = 0;
-	        gbc.weightx = 0.0;
-	        gbc.gridwidth = 3;
-	        p.add(new JLabel("Aciklama:" ), gbc);
-	        
-	        gbc.gridx = 1;
-	        p.add(new JLabel(acikLAMA ), gbc);
-	        
-	        
-	        gbc.gridy = 4;
-	        gbc.gridx = 0;
-	        gbc.weightx = 0.0;
-	        gbc.gridwidth = 3;
-	        p.add(new JLabel( ), gbc);
-	        
-	        gbc.gridx = 0;
-	        p.add(new JLabel("Durum" ), gbc);
-	        gbc.gridx = 1;
-	        p.add(new JLabel(durUM), gbc);
-	        
-	        gbc.gridy = 6;
-	        gbc.gridx = 0;
-	        JButton btnNewButton = new JButton("Emir Duzelt");
-			btnNewButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
+		gbc.gridx = 1;
+		p.add(new JLabel(Integer.toString(dosyaSayisi) + " Adet Dosya" ), gbc);
 
-					timer.cancel();
-					timer.purge();
-					VT_ANA_CLASS.yENI_EMIR = false ;
-					VT_ANA_CLASS.EMIR_ADI= emirAdi;
-					EMIR emr = new EMIR();
-					emr.setVisible(true);
-					//   System.out.println("="+BASLA.gorevLER.size());
-					//	BASLA.gorevLER.add(emirAdi);
-					//   System.out.println("==" +BASLA.gorevLER.size());
-				}
-			});
-	        p.add(btnNewButton, gbc);
-	        rss = sqll.yedeklemeBILGI(  ((javax.swing.border.TitledBorder) p.getBorder()).getTitle());
-	        
-	        sonRAKI_YEDEK(Integer.parseInt(rss.getString("SAAT")));
-	        
-	       basla();
-	        return p;
-	    }
+		gbc.gridx = 2;
+		p.add(new JLabel("          Gelecek Yedekleme :" ), gbc);
+		gbc.gridx = 3;
+
+		lblgel = new JLabel(gelYEDEK );
+		lblgel.setForeground(Color.RED);
+		p.add(lblgel, gbc);
+
+		gbc.gridy = 2;
+		gbc.gridx = 0;
+		gbc.weightx = 0.0;
+		gbc.gridwidth = 2;
+		p.add(new JLabel("Surucu / FTP  :" ), gbc);
+
+		gbc.gridx = 1;
+		p.add(new JLabel(surUCU), gbc);
+
+		gbc.gridy = 3;
+		gbc.gridx = 0;
+		gbc.weightx = 0.0;
+		gbc.gridwidth = 3;
+		p.add(new JLabel("Aciklama:" ), gbc);
+
+		gbc.gridx = 1;
+		p.add(new JLabel(acikLAMA ), gbc);
+
+
+		gbc.gridy = 4;
+		gbc.gridx = 0;
+		gbc.weightx = 0.0;
+		gbc.gridwidth = 3;
+		p.add(new JLabel( ), gbc);
+
+		gbc.gridx = 0;
+		p.add(new JLabel("Durum" ), gbc);
+		gbc.gridx = 1;
+		p.add(new JLabel(durUM), gbc);
+
+		gbc.gridy = 6;
+		gbc.gridx = 0;
+		JButton btnNewButton = new JButton("Emir Duzelt");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				timer.cancel();
+				timer.purge();
+				VT_ANA_CLASS.yENI_EMIR = false ;
+				VT_ANA_CLASS.EMIR_ADI= emirAdi;
+				EMIR emr = new EMIR();
+				emr.setVisible(true);
+				//   System.out.println("="+BASLA.gorevLER.size());
+				//	BASLA.gorevLER.add(emirAdi);
+				//   System.out.println("==" +BASLA.gorevLER.size());
+			}
+		});
+		p.add(btnNewButton, gbc);
+		rss = sqll.yedeklemeBILGI(  ((javax.swing.border.TitledBorder) p.getBorder()).getTitle());
+		System.out.println("baslama");   
+		sonRAKI_YEDEK(Integer.parseInt(rss.getString("SAAT")));
+
+		basla();
+		return p;
+	}
 	public static void basla() throws InterruptedException
 	{
 		TimerTask timerTask = new TimerTask() {
@@ -175,8 +170,9 @@ public class GOREV {
 			timer.purge();
 		}
 	}
-	
-	
+
+
+	@SuppressWarnings("deprecation")
 	private static void   sonRAKI_YEDEK(int kacDAKKA) throws NumberFormatException, SQLException, ParseException, InterruptedException, ClassNotFoundException
 	{
 		DateFormat dff = new SimpleDateFormat("dd.MM.yyyy HH:mm");
@@ -213,11 +209,11 @@ public class GOREV {
 				long suANL ;
 				suAN.setSeconds(0);
 				suANL = suAN.getTime();
-				System.out.println(new Date(i) +"==+==" + suAN);
+				//System.out.println(new Date(i) +"==+==" + suAN);
 				if ( i > suANL)
 				{
 					Date currentDate = new Date(i);
-					System.out.println("burda=" + currentDate);
+					//System.out.println("burda=" + currentDate);
 					DateFormat  df = new SimpleDateFormat("dd.MM.yyyy HH:mm");
 					lblgel.setText(df.format(currentDate));
 					break ;
@@ -226,255 +222,242 @@ public class GOREV {
 		}
 	}
 
-private static void gunLERE_BAK() throws SQLException, ParseException, InterruptedException
-{
-	LocalDate date = LocalDate.now();
-	DayOfWeek dayOfWeek = date.getDayOfWeek();
-	int hangiGUNDEYIZ = dayOfWeek.getValue(); // 6
-	//String dayOfWeekName = dayOfWeek.name(); // SATURDAY
-	if(hangiGUNDEYIZ == 1)
+	private static void gunLERE_BAK() throws SQLException, ParseException, InterruptedException
 	{
-		 hangiGUN_P_TESI();
-	}
-	else if(hangiGUNDEYIZ == 2)
-	{
-	
-		 hangiGUN_SALI();
-	}
-	if(hangiGUNDEYIZ == 3)
-	{
-		 hangiGUN_CARS();
-	}
-	if(hangiGUNDEYIZ == 4)
-	{
-		 hangiGUN_PERS();
-	}
-	if(hangiGUNDEYIZ == 5)
-	{
-		 hangiGUN_CUMA();
-	}
-	if(hangiGUNDEYIZ == 6)
-	{
-		 hangiGUN_C_TESI();
-	}
-	if(hangiGUNDEYIZ == 6)
-	{
-		 hangiGUN_PAZAR();
-	}
-}
-private static void hangiGUN_P_TESI() throws SQLException, ParseException, InterruptedException
-{
-	if (rss.getBoolean("P_TESI"))
-	{
-		for(int i= 4 ; i < 10; i++)
+		LocalDate date = LocalDate.now();
+		DayOfWeek dayOfWeek = date.getDayOfWeek();
+		hangiGUNDEYIZ = dayOfWeek.getValue(); // 6
+		//String dayOfWeekName = dayOfWeek.name(); // SATURDAY
+		varmi = false ;
+		if(hangiGUNDEYIZ == 1)
 		{
-			if(rss.getBoolean(i))
-			{
-				varmi = true;
-				gunSAYISI =0 ;
-				gunEKLE(rss.getDate("BASLAMA") ,i-3); // Sali 4
-				break ;
-			}
+			hangiGUN_P_TESI();
 		}
-		if (varmi ==false)
+		else if(hangiGUNDEYIZ == 2)
 		{
-			gunEKLE(rss.getDate("BASLAMA") ,7);
+			hangiGUN_SALI();
+		}
+		if(hangiGUNDEYIZ == 3)
+		{
+			hangiGUN_CARS();
+		}
+		if(hangiGUNDEYIZ == 4)
+		{
+			hangiGUN_PERS();
+		}
+		if(hangiGUNDEYIZ == 5)
+		{
+			hangiGUN_CUMA();
+		}
+		if(hangiGUNDEYIZ == 6)
+		{
+			hangiGUN_C_TESI();
+		}
+		if(hangiGUNDEYIZ == 6)
+		{
+			hangiGUN_PAZAR();
 		}
 	}
-	else
-	{  //   BUGUN YOK DIGER GUN BAK 
-	hangiGUN_SALI();
-	}
-}
-private static void hangiGUN_SALI() throws SQLException, ParseException, InterruptedException
-{
-	if (rss.getBoolean("SALI"))
+	private static void hangiGUN_P_TESI() throws SQLException, ParseException, InterruptedException
 	{
-		for(int i= 5 ; i < 10; i++)
+		if (rss.getBoolean("P_TESI"))
 		{
-			if(rss.getBoolean(i))
+			for(int i= 4 ; i < 10; i++)
 			{
-				varmi = true;
-				gunSAYISI =0 ;
-				gunEKLE(rss.getDate("BASLAMA") ,i-4); // cars 5
-				break ;
-			}
-		}
-		if (varmi ==false)
-		{
-			for(int i= 3 ; i < 5; i++)
-			{
-					if(rss.getBoolean(i))
+				if(rss.getBoolean(i))
 				{
 					varmi = true;
-			
-					gunEKLE(rss.getDate("BASLAMA") ,i+2);
+					gunEKLE(rss.getDate("BASLAMA") ,(i-2) - hangiGUNDEYIZ);
 					break ;
 				}
 			}
+			if (varmi ==false)
+			{
+				gunEKLE(rss.getDate("BASLAMA") ,7);
+			}
+		}
+		else
+		{  //   BUGUN YOK DIGER GUN BAK 
+			hangiGUN_SALI();
 		}
 	}
-	else
+	private static void hangiGUN_SALI() throws SQLException, ParseException, InterruptedException
 	{
-		hangiGUN_CARS();
+		if (rss.getBoolean("SALI"))
+		{
+			for(int i= 5 ; i < 10; i++)
+			{
+				if(rss.getBoolean(i))
+				{
+					varmi = true;
+					gunEKLE(rss.getDate("BASLAMA") ,i-4); // cars 5
+					break ;
+				}
+			}
+			if (varmi ==false)
+			{
+				for(int i= 3 ; i < 5; i++)
+				{
+					if(rss.getBoolean(i))
+					{
+						varmi = true;
+						gunEKLE(rss.getDate("BASLAMA") ,(i-2) - hangiGUNDEYIZ);
+						break ;
+					}
+				}
+			}
+		}
+		else
+		{
+			//System.out.println("318");
+			hangiGUN_CARS();
+		}
 	}
-}
-private static void hangiGUN_CARS() throws ParseException, InterruptedException, SQLException
-{
-	if (rss.getBoolean("CARS"))
+	private static void hangiGUN_CARS() throws ParseException, InterruptedException, SQLException
 	{
+		if (rss.getBoolean("CARS"))
+		{
 			for(int i= 6 ; i < 10; i++)
-		{
+			{
 				if(rss.getBoolean(i))
-			{
-				varmi = true;
-				gunSAYISI =0 ;
-				gunEKLE(rss.getDate("BASLAMA") ,i - 5); //pers 6
-				break ;
+				{
+					varmi = true;
+					gunEKLE(rss.getDate("BASLAMA") ,i - 5); //pers 6
+					break ;
+				}
 			}
-		}
-		if (varmi ==false)
-		{
-			for(int i= 3 ; i <5; i++)
+			if (varmi ==false)
 			{
+				for(int i= 3 ; i <5; i++)
+				{
 					if(rss.getBoolean(i))
-				{
-					varmi = true;
-					gunEKLE(rss.getDate("BASLAMA") ,i-2);
-					break ;
+					{
+						varmi = true;
+						gunEKLE(rss.getDate("BASLAMA") ,(i-2) - hangiGUNDEYIZ);
+						break ;
+					}
 				}
 			}
 		}
-	}
-	else
-	{
-		hangiGUN_PERS();
-	}
-}
-private static void hangiGUN_PERS() throws SQLException, ParseException, InterruptedException
-{
-	if (rss.getBoolean("PERS"))
-	{
-		for(int i= 7 ; i < 10; i++)
+		else
 		{
-			if(rss.getBoolean(i))
-			{
-				varmi = true;
-				gunSAYISI =0 ;
-				gunEKLE(rss.getDate("BASLAMA") ,i -6); //cuma 7
-				break ;
-			}
+			hangiGUN_PERS();
 		}
-		if (varmi ==false)
+	}
+	private static void hangiGUN_PERS() throws SQLException, ParseException, InterruptedException
+	{
+		if (rss.getBoolean("PERS"))
 		{
-			for(int i= 3 ; i < 6; i++)
+			for(int i= 7 ; i < 10; i++)
 			{
 				if(rss.getBoolean(i))
 				{
 					varmi = true;
-					gunEKLE(rss.getDate("BASLAMA") ,i-2);
+					gunEKLE(rss.getDate("BASLAMA") ,i -6); //cuma 7
 					break ;
 				}
 			}
-		}
-	}
-	else
-	{
-		hangiGUN_CUMA();
-	}
-}
-private static void hangiGUN_CUMA() throws SQLException, ParseException, InterruptedException
-{
-	if (rss.getBoolean("CUMA"))
-	{
-			for(int i= 8 ; i < 9; i++)
-		{
-			if(rss.getBoolean(i))
+			if (varmi ==false)
 			{
-				varmi = true;
-				gunSAYISI =0 ;
-				gunEKLE(rss.getDate("BASLAMA") ,i - 7); //c.tesi 8
-				break ;
+				for(int i= 3 ; i < 6; i++)
+				{
+					if(rss.getBoolean(i))
+					{
+						varmi = true;
+						gunEKLE(rss.getDate("BASLAMA") ,(i-2) - hangiGUNDEYIZ);
+						break ;
+					}
+				}
 			}
 		}
-		if (varmi ==false)
+		else
 		{
-			for(int i= 3 ; i < 7; i++)
+			System.out.println("386");
+			hangiGUN_CUMA();
+		}
+	}
+	private static void hangiGUN_CUMA() throws SQLException, ParseException, InterruptedException
+	{
+		if (rss.getBoolean("CUMA"))
+		{
+			for(int i= 8 ; i <10; i++)
 			{
 				if(rss.getBoolean(i))
 				{
 					varmi = true;
-					gunEKLE(rss.getDate("BASLAMA") ,i-2);
+					gunEKLE(rss.getDate("BASLAMA") ,i - 7); //c.tesi 8
 					break ;
 				}
 			}
-		}
-	}
-	else
-	{
-		hangiGUN_C_TESI();
-	}
-}
-private static void hangiGUN_C_TESI() throws SQLException, ParseException, InterruptedException
-{
-	if (rss.getBoolean("C_TESI"))
-	{
-		for(int i= 9 ; i < 10; i++)
-		{
-			if(rss.getBoolean(i))
+			if (varmi ==false)
 			{
-				varmi = true;
-				gunSAYISI =0 ;
-				gunEKLE(rss.getDate("BASLAMA") ,i-8); // pazar 9  
-				break ;
+				for(int i= 3 ; i < 8; i++)
+				{
+					if(rss.getBoolean(i))
+					{
+						varmi = true;
+						//System.out.println(" 413= " +(i-2)  + "===" + hangiGUNDEYIZ );
+						gunEKLE(rss.getDate("BASLAMA") , (i-2) - hangiGUNDEYIZ);
+						break ;
+					}
+				}
 			}
 		}
-		if (varmi ==false)
+		else
 		{
-			for(int i= 3 ; i < 8; i++)
+			//System.out.println("420");
+			hangiGUN_C_TESI();
+		}
+	}
+	private static void hangiGUN_C_TESI() throws SQLException, ParseException, InterruptedException
+	{
+		if (rss.getBoolean("C_TESI"))
+		{
+			for(int i= 9 ; i < 10; i++)
 			{
 				if(rss.getBoolean(i))
 				{
 					varmi = true;
-					gunEKLE(rss.getDate("BASLAMA") ,i-1);
+					gunEKLE(rss.getDate("BASLAMA") ,i-8); // pazar 9  
+					break ;
+				}
+			}
+			if (varmi ==false)
+			{
+				for(int i= 3 ; i < 9; i++)
+				{
+					if(rss.getBoolean(i))
+					{
+						varmi = true;
+						gunEKLE(rss.getDate("BASLAMA") ,(i-2) - hangiGUNDEYIZ);
+						break ;
+					}
+				}
+			}
+		}
+		else
+		{
+			System.out.println("451");
+			hangiGUN_PAZAR();
+		}
+	}
+	private static void hangiGUN_PAZAR() throws SQLException, ParseException, InterruptedException
+	{
+		if (rss.getBoolean("PAZAR"))
+		{
+			for(int i= 3 ; i < 10; i++){
+				if(rss.getBoolean(i ))
+				{
+					gunEKLE(rss.getDate("BASLAMA") ,(i-2) - hangiGUNDEYIZ);
 					break ;
 				}
 			}
 		}
-	}
-	else
-	{
-		hangiGUN_PAZAR();
-	}
-}
-private static void hangiGUN_PAZAR() throws SQLException, ParseException, InterruptedException
-{
-	if (rss.getBoolean("PAZAR"))
-	{
-		for(int i= 3 ; i < 10; i++){
-			if(rss.getBoolean(i ))
-			{
-				gunSAYISI =0 ;
-				gunEKLE(rss.getDate("BASLAMA") ,i-2);
-				break ;
-			}
+		else
+		{
+			gunEKLE(rss.getDate("BASLAMA") ,0);
 		}
 	}
-	else
-	{
-		/////
-		gunEKLE(rss.getDate("BASLAMA") ,0);
-//		for(int i= 3 ; i < 9; i++){
-//			if(rss.getBoolean(i ))
-//			{
-//				System.out.println("470" + gunSAYISI  + "===" + (i-2)) ;
-//			gunEKLE(rss.getDate("BASLAMA") ,i-2);
-//				break ;
-//			}
-//		}
-		////
-	}
-}
 	@SuppressWarnings("deprecation")
 	private static void gunEKLE(Date basLA, int gun) throws ParseException, InterruptedException
 	{
@@ -482,29 +465,25 @@ private static void hangiGUN_PAZAR() throws SQLException, ParseException, Interr
 		String simDI = df.format(basLA);
 		Date date1=new SimpleDateFormat("dd.MM.yyyy HH:mm").parse(simDI);  
 
-
 		Date dtt = new Date();
 		Calendar c = Calendar.getInstance(); 
 		c.setTime(dtt); 
-		c.add(Calendar.DATE, gun + gunSAYISI);
+		c.add(Calendar.DATE, gun );
 		dtt = c.getTime();
 
-		System.out.println("481=" + gun);
+
 		dtt.setHours(date1.getHours());
 		dtt.setMinutes(date1.getMinutes());
 		dtt.setSeconds(0);
 
 		df = new SimpleDateFormat("dd.MM.yyyy HH:mm");
-	
+
 		lblgel.setText(df.format(dtt));
-		 basla();
+		basla();
 	}
 	private static void yedekLE()
 	{
 		System.out.println("Yedekleme..");  
 	}
-	private void  guniciAYARLAMA()
-	{
-		
-	}
-      }
+
+}

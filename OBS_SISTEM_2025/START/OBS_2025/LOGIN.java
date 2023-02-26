6b56d9b5a7d591795d5754a3fa7b22d6f17ab494
Hamit <hamit@okumus.com>
2023-02-26 21:01:33 +0200

    1

--- OBS_SISTEM_2025/START/OBS_2025/LOGIN.java
+++ OBS_SISTEM_2025/START/OBS_2025/LOGIN.java
@@ -412,11 +412,7 @@ public class LOGIN extends JFrame {
 				JOptionPane.showMessageDialog(null, "Calisilan Cari -" + BAGLAN.cariDizin.kOD + "- Nolu Dosya Bulunamadi.....",  "Dosya Baglanti", JOptionPane.ERROR_MESSAGE);        
 				qwe = BAGLAN.cariDizin.yER.equals("S") ?  BAGLAN.cariDizin.sERVER : "Lokal" ;
 				OBS_MAIN.lblNewLabel_1.setText ( "Cari:" + BAGLAN.cariDizin.kOD + "/ " + BAGLAN.cariDizin.fIRMA_ADI + "/ " + qwe );
-<<<<<<< Upstream, based on branch 'master' of https://github.com/hamitodessa/Eclipse.git
 				OBS_MAIN.tabbedPane.setEnabledAt(0,false);
-=======
-				OBS_MAIN.tabbedPane.setEnabledAt(0, false);
->>>>>>> 49360bf 1
 			}
 			else 
 			{ 

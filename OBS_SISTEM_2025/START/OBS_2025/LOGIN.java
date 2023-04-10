e9ae26717c7d9a1bd1f33823635705208ce4fef1
Hamit <hamit@okumus.com>
2023-04-11 01:34:40 +0300

    1

--- OBS_SISTEM_2025/START/OBS_2025/LOGIN.java
+++ OBS_SISTEM_2025/START/OBS_2025/LOGIN.java
@@ -367,6 +367,19 @@ public class LOGIN extends JFrame {
 					}
 				}
 			}
+			try {
+	            //here you can put the selected theme class name in JTattoo
+	            UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
+	 
+	        } catch (ClassNotFoundException ex) {
+	            java.util.logging.Logger.getLogger(LOGIN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (InstantiationException ex) {
+	            java.util.logging.Logger.getLogger(LOGIN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (IllegalAccessException ex) {
+	            java.util.logging.Logger.getLogger(LOGIN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+	            java.util.logging.Logger.getLogger(LOGIN.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        }
 		} 
 		catch (Exception ex) 
 		{

c8685aee0cbf5fede96f0c08927e491e09490b23
hamit <hamit@okumus.com>
2023-04-14 17:10:33 +0300

    1

--- OBS_CLASS_2025/YARDIMCI_CLASS/OBS_C_2025/COLUMN_RENDERER.java
+++ OBS_CLASS_2025/YARDIMCI_CLASS/OBS_C_2025/COLUMN_RENDERER.java
@@ -5,41 +5,49 @@ import java.awt.Component;
 import java.awt.Font;
 import java.util.Date;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.border.Border;
 import javax.swing.table.DefaultTableCellRenderer;
 
-public class COLUMN_RENDERER  extends DefaultTableCellRenderer {
-		   Color backgroundColor, foregroundColor;
-		   public COLUMN_RENDERER(Color backgroundColor, Color foregroundColor) {
-		      super();
-		      this.backgroundColor = backgroundColor;
-		      this.foregroundColor = foregroundColor;
-		   }
-		   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,   boolean hasFocus, int row, int column) {
-		      Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-
-		    	  if( value.toString().length() != 0 )
-			      {
-			    	  cell.setBackground(backgroundColor);
-				      cell.setForeground(foregroundColor); 
-				      Font fnt = new Font(table.getFont().getFontName(), 1 ,10);
-					cell.setFont(fnt);
-			      } 
-		    	  else 
-		    	  {
-		    		cell.setBackground(Color.WHITE);
-					cell.setForeground(Color.WHITE);
-				  }
-		    
-		      
-		      
-		      if (column ==0)
-		      {
-		      setHorizontalAlignment( JLabel.CENTER );
-		      }
-		      
-		      return cell;
-		   }
+@SuppressWarnings("serial")
+public class COLUMN_RENDERER  extends DefaultTableCellRenderer 
+{
+	Color backgroundColor, foregroundColor;
+	public COLUMN_RENDERER(Color backgroundColor, Color foregroundColor) 
+	{
+		super();
+		this.backgroundColor = backgroundColor;
+		this.foregroundColor = foregroundColor;
+	}
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,   boolean hasFocus, int row, int column) 
+	{
+		Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		if( value.toString() != ""  )
+		{
+			if (column !=0)
+			{
+				cell.setBackground(backgroundColor);
+				cell.setForeground(foregroundColor); 
+				Font fnt = new Font(table.getFont().getFontName(), 1 ,10);
+				cell.setFont(fnt);
+				Border border = BorderFactory.createMatteBorder(1, 0, 1, 0, new Color(0, 60, 102));
+				setBorder(border);
+			}
+			else if (column ==0)
+			{
+				setHorizontalAlignment( JLabel.CENTER );
+				cell.setBackground(backgroundColor);
+				cell.setForeground(foregroundColor); 
+			}
+		} 
+		else 
+		{
+			cell.setBackground(Color.WHITE);
+			cell.setForeground(Color.WHITE);
 		}
+		return cell;
+	}
+}
 

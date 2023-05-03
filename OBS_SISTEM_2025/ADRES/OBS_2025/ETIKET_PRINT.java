01104160a47dea3dd47e72ef4fdc36fe2f240fe9
hamit <hamit@okumus.com>
2023-05-03 17:46:06 +0300

    1

--- OBS_SISTEM_2025/ADRES/OBS_2025/ETIKET_PRINT.java
+++ OBS_SISTEM_2025/ADRES/OBS_2025/ETIKET_PRINT.java
@@ -171,40 +171,28 @@ public class ETIKET_PRINT extends JInternalFrame {
 			}
 		}
 	}
-	@SuppressWarnings("null")
 	public static ByteArrayDataSource export_to(String forMAT) throws IOException, JRException
 	{
+		 //JasperExportManager.exportReportToPdfFile(jp, "C:\\OBS_SISTEM\\invoice.pdf");
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		ByteArrayInputStream byteArrayInputStream = null;
 		ByteArrayDataSource ds = null ;
 		MimeBodyPart messagePart = null ;
 		InputStream inputStream = null ;
 		
-		//JasperExportManager.exportReportToPdfStream(jp,byteArrayOutputStream);
-
-		//
-		JRPdfExporter exporter = new JRPdfExporter();
-		exporter.setExporterInput(new SimpleExporterInput(jp));
-		exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(byteArrayOutputStream));
-		exporter.exportReport();
-		//
-		
-		byte[] byteArray = byteArrayOutputStream.toByteArray();
-		
-		//ByteArrayInputStream bais = new ByteArrayInputStream(byteArray);
-	
-		byteArrayInputStream = new ByteArrayInputStream(byteArray);
-		
-		System.out.println("=="+byteArrayInputStream.available());
-		
-		byteArrayOutputStream = new ByteArrayOutputStream(byteArrayInputStream.available());
-		
-		int x = byteArrayInputStream.read(byteArray, 0, byteArrayInputStream.available());
-		byteArrayOutputStream.write(byteArray, 0, x);
-		byteArrayInputStream.close();
+		JasperExportManager.exportReportToPdfStream(jp,byteArrayOutputStream);
 		byteArrayOutputStream.close();
-		inputStream = new ByteArrayInputStream(byteArrayOutputStream .toByteArray());
-		ds = new ByteArrayDataSource(inputStream, "application/x-any");
+		//
+		//JRPdfExporter exporter = new JRPdfExporter();
+		//exporter.setExporterInput(new SimpleExporterInput(jp));
+		//exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(byteArrayOutputStream));
+		//exporter.exportReport();
+		//
+		
+		byte byteArray[] = byteArrayOutputStream.toByteArray();
+		InputStream is = new ByteArrayInputStream(byteArray);
+		
+		ds = new ByteArrayDataSource(is, "application/x-any");
 		return ds;
 		
 

4176037d354c1641c5fc72ceef8a5acd425bce98
hamit <hamit@okumus.com>
2023-10-28 15:22:36 +0300

    1

--- OBS_SISTEM_2025/START/OBS_2025/OBS_MAIN.java
+++ OBS_SISTEM_2025/START/OBS_2025/OBS_MAIN.java
@@ -13,11 +13,6 @@ import java.util.TimerTask;
 import javax.swing.JDesktopPane;
 import java.awt.Toolkit;
 import javax.swing.JButton;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.TargetDataLine;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JToolBar;
@@ -234,15 +229,7 @@ public class OBS_MAIN extends JFrame {
 	static OBS_SIS_2025_ANA_CLASS oac = new OBS_SIS_2025_ANA_CLASS();
 	private static GUNLUK_ACCESS  g_Access = new GUNLUK_ACCESS(oac._IGunluk , oac._IGunluk_Loger);
 	
-	 static AudioFormat getAudioFormat() {
-	        float sampleRate = 16000;
-	        int sampleSizeInBits = 8;
-	        int channels = 2;
-	        boolean signed = true;
-	        boolean bigEndian = true;
-	        return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed,
-	                bigEndian);
-	    }
+
 
 	public OBS_MAIN() {
 //2663 satir
@@ -3646,6 +3633,7 @@ public class OBS_MAIN extends JFrame {
 	}
 	private static void gunluk_goster()
 	{
+		InputStream stream = null ;
 		try {
 			GuiUtil.setWaitCursor(toolBar,true);
 			Gunluk_Bilgi gbilgi = new Gunluk_Bilgi();
@@ -3665,33 +3653,28 @@ public class OBS_MAIN extends JFrame {
 				GuiUtil.setWaitCursor(toolBar,false);
 				return; // Kayit Yok
 			} 
-			InputStream stream = OBS_MAIN.class.getClassLoader().getResourceAsStream("DOSYA/Whatsap.mp3"); //whts
+			stream = OBS_MAIN.class.getClassLoader().getResourceAsStream("DOSYA/Whatsap.mp3"); //whts
 			Player player = new Player(stream);
 			while (rs.next()) 
 			{
 				String html = TARIH_CEVIR.tarih_ters(rs.getString("TARIH")) + "  " + rs.getString("SAAT")  + "  "
 						+ rs.getString("ISIM")  + "  " + rs.getString("GOREV")  + "  "
 						+ rs.getString("MESAJ");
-				///
-				 TargetDataLine audioLine;
-			     AudioFormat format;
-				format = getAudioFormat();
-		        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-		 
-		        // checks if system supports the data line
-		        if (AudioSystem.isLineSupported(info)) {
-					player.play();
-					Notifications.getInstance().show(Notifications.Type.INFO,Notifications.Location.BOTTOM_RIGHT ,20000 ,String.format(html));
-
-		        }
-				
-				///
+				Notifications.getInstance().show(Notifications.Type.INFO,Notifications.Location.BOTTOM_RIGHT ,20000 ,String.format(html));
 			}
+			player.play();
 			stream.close();
 			GuiUtil.setWaitCursor(toolBar,false);
 		} catch (Exception e) {
 			GuiUtil.setWaitCursor(toolBar,false);
-			JOptionPane.showMessageDialog(null,e.getMessage(), "Gunluk Kontrol", JOptionPane.ERROR_MESSAGE);
+			try {
+				stream.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			Notifications.getInstance().show(Notifications.Type.INFO,Notifications.Location.BOTTOM_RIGHT ,10000 ,e.getMessage());
+
+			//JOptionPane.showMessageDialog(null,e.getMessage(), "Gunluk Kontrol", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 	private static long millisToNextHour(Calendar calendar) 

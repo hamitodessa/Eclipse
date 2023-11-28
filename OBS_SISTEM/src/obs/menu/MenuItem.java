86a6702b785e563b6f66a703acce5903bd2b64f5
hamit <hamit@okumus.com>
2023-11-29 00:44:09 +0200

    1

--- OBS_SISTEM/src/obs/menu/MenuItem.java
+++ OBS_SISTEM/src/obs/menu/MenuItem.java
@@ -79,7 +79,8 @@ public class MenuItem extends JPanel {
     private Icon getIcon() {
         Color lightColor = FlatUIUtils.getUIColor("Menu.icon.lightColor", Color.red);
         Color darkColor = FlatUIUtils.getUIColor("Menu.icon.darkColor", Color.red);
-        FlatSVGIcon icon = new FlatSVGIcon("obs/menu/icon/" + menuIndex + ".svg");
+        //FlatSVGIcon icon = new FlatSVGIcon("obs/menu/icon/" + menuIndex + ".svg");
+        FlatSVGIcon icon = new FlatSVGIcon("obs/menu/icon/rehber1.svg");
         FlatSVGIcon.ColorFilter f = new FlatSVGIcon.ColorFilter();
         f.add(Color.decode("#969696"), lightColor, darkColor);
         icon.setColorFilter(f);

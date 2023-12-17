4696da06240a173f35e78095a1abac948aecb87c
hamit <hamit@okumus.com>
2023-12-17 12:52:14 +0200

    1

---
+++ OBS_BACKUP/src/obs/backup/main/OBS_BACKUP.java
@@ -0,0 +1,58 @@
+package obs.backup.main;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+
+public class OBS_BACKUP extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					OBS_BACKUP frame = new OBS_BACKUP();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public OBS_BACKUP() {
+		FlatRobotoFont.install();
+		FlatLaf.registerCustomDefaultsSource("obs.backup.theme");
+		UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
+		FlatMacDarkLaf.setup();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JSplitPane splitPane = new JSplitPane();
+		contentPane.add(splitPane);
+	}
+
+}

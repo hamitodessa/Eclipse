b38f83695b5a526b9be349c6099e9ec1ba6206f5
hamit <hamit@okumus.com>
2024-01-12 11:21:01 +0300

    1

--- OBS_CLASS_2025/YARDIMCI_CLASS/OBS_C_2025/Obs_TextFIeld.java
+++ OBS_CLASS_2025/YARDIMCI_CLASS/OBS_C_2025/Obs_TextFIeld.java
@@ -7,10 +7,23 @@ import com.formdev.flatlaf.FlatClientProperties;
 @SuppressWarnings("serial")
 public class Obs_TextFIeld  extends JTextField{
 	
+	public Obs_TextFIeld()
+	{
+		putClientProperty(FlatClientProperties.TEXT_FIELD_SHOW_CLEAR_BUTTON, true);
+		JTextFieldRegularPopupMenu.addTo(this);
+		
+	}
 	public Obs_TextFIeld(int limit,String plcHolderText)
 	{
 		putClientProperty(FlatClientProperties.TEXT_FIELD_SHOW_CLEAR_BUTTON, true);
 		putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, plcHolderText);
+		JTextFieldRegularPopupMenu.addTo(this);
+		setDocument(new JTextFieldLimit(limit));
+	}
+	
+	public Obs_TextFIeld(int limit)
+	{
+		putClientProperty(FlatClientProperties.TEXT_FIELD_SHOW_CLEAR_BUTTON, true);
 		JTextFieldRegularPopupMenu.addTo(this);
 		setDocument(new JTextFieldLimit(limit));
 	}

3509de8df38b9061fe190d553481500ac1e0b2a5
hamit <hamit@okumus.com>
2024-03-07 10:36:11 +0200

    1

--- OBS_SISTEM_2025/CARI_HESAP/OBS_2025/TAH_DOKUM.java
+++ OBS_SISTEM_2025/CARI_HESAP/OBS_2025/TAH_DOKUM.java
@@ -1,10 +1,8 @@
 package OBS_2025;
 
-import javax.swing.JComboBox;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
-import javax.swing.border.LineBorder;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
 import javax.swing.event.ListSelectionEvent;
@@ -13,8 +11,6 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
-
-import com.toedter.calendar.JDateChooser;
 
 import OBS_C_2025.CARI_ACCESS;
 import OBS_C_2025.FORMATLAMA;
@@ -37,23 +33,26 @@ import java.awt.event.KeyEvent;
 import java.sql.ResultSet;
 
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
+import javax.swing.border.LineBorder;
 import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 @SuppressWarnings({"serial", "static-access","unused" })
 public class TAH_DOKUM extends JInternalFrame {
 
 	private static final long serialVersionUID = 1L;
 	static OBS_SIS_2025_ANA_CLASS oac = new OBS_SIS_2025_ANA_CLASS();
-
 	private static CARI_ACCESS  c_Access = new CARI_ACCESS(OBS_SIS_2025_ANA_CLASS._ICar , OBS_SIS_2025_ANA_CLASS._ICari_Loger);
 	private static JTable table;
 	public static JSplitPane splitPane;
 	private static JTable table_1;
+	private static JLabel lblToplam ;
+	private JLabel lblNewLabel;
+	private static JLabel lblKayit;
 
-	
 	public TAH_DOKUM() {
 		setTitle("TAHSILAT DOKUM");
 		setResizable(true);
@@ -63,46 +62,35 @@ public class TAH_DOKUM extends JInternalFrame {
 		setBounds(0,0, 900, 600);
 
 		splitPane = new JSplitPane();
+		splitPane.setResizeWeight(1.0);
 		splitPane.setDividerSize(0);
-		splitPane.setResizeWeight(0.0);
 		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
 		getContentPane().add(splitPane, BorderLayout.CENTER);
 
-		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(null));
-		splitPane.setLeftComponent(panel);
-		panel.setMinimumSize(new Dimension(0, 30));
-		panel.setMaximumSize(new Dimension(0, 30));
-		panel.setLayout(null);
+		ScrollPaneWin11 scrollPane_1 = new ScrollPaneWin11();
+		scrollPane_1.setMinimumSize(new Dimension(0, 100));
+		scrollPane_1.setMaximumSize(new Dimension(0, 100));
 
-		JSplitPane splitPane_1 = new JSplitPane();
-		splitPane_1.setResizeWeight(1.0);
-		splitPane_1.setOrientation(JSplitPane.VERTICAL_SPLIT);
-		splitPane.setRightComponent(splitPane_1);
+		splitPane.setRightComponent(scrollPane_1);
 
-		ScrollPaneWin11 scrollPane = new ScrollPaneWin11();
-		splitPane_1.setLeftComponent(scrollPane);
-
-		table = new JTable(){
-			public boolean isCellEditable(int row, int column) {     return false;          }
-			
+		table = new JTable()
+		{
+			public boolean isCellEditable(int row, int column) {return false;}
 		};
-		scrollPane.setViewportView(table);
-		
 		table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-		    public void valueChanged(ListSelectionEvent lse) {
-			        if (!lse.getValueIsAdjusting()) {
-			        	 DefaultTableModel model = (DefaultTableModel)table.getModel();
-			        	 if (model.getRowCount() == 0) return ;
-			        	 if (table.getSelectedRow()  < 0) return;
-			        	 table.setCursor(oac.WAIT_CURSOR);//
-			        	 GRID_TEMIZLE.grid_temizle(table_1);
-			        	 if(model.getValueAt(table.getSelectedRow() , 5).toString().equals("Cek"))
-			        		 detay_doldur(model.getValueAt(table.getSelectedRow() , 0).toString(),model.getValueAt(table.getSelectedRow() , 4).toString());
-			        	 table.setCursor(oac.DEFAULT_CURSOR);
-			        }
-			    }
-			});
+			public void valueChanged(ListSelectionEvent lse) {
+				if (!lse.getValueIsAdjusting()) {
+					DefaultTableModel model = (DefaultTableModel)table.getModel();
+					if (model.getRowCount() == 0) return ;
+					if (table.getSelectedRow()  < 0) return;
+					table.setCursor(oac.WAIT_CURSOR);//
+					GRID_TEMIZLE.grid_temizle(table_1);
+					if(model.getValueAt(table.getSelectedRow() , 5).toString().equals("Cek"))
+						detay_doldur(model.getValueAt(table.getSelectedRow() , 0).toString(),model.getValueAt(table.getSelectedRow() , 4).toString());
+					table.setCursor(oac.DEFAULT_CURSOR);
+				}
+			}
+		});
 
 		table.addAncestorListener(new AncestorListener() {
 			public void ancestorAdded(AncestorEvent event) {
@@ -121,11 +109,9 @@ public class TAH_DOKUM extends JInternalFrame {
 		if(! oac.gridcolor.toString().equals("java.awt.Color[r=255,g=255,b=255]"))
 			table.setGridColor(oac.gridcolor);
 		table.addKeyListener(new KeyAdapter() {
-			
 			@Override
 			public void keyPressed(KeyEvent e) {
 				try {
-
 					String[] parts;
 					String deger ;
 					deger = GLOBAL.setting_oku("PRG_FILTRE").toString();
@@ -152,14 +138,40 @@ public class TAH_DOKUM extends JInternalFrame {
 		table.setSurrendersFocusOnKeystroke(true);
 		table.setShowHorizontalLines(true);
 		table.setShowVerticalLines(true);
-		
-		ScrollPaneWin11 scrollPane_1 = new ScrollPaneWin11();
-		//scrollPane_1.setBorder(new LineBorder(null));
-		scrollPane_1.setMinimumSize(new Dimension(0, 100));
-		scrollPane_1.setMaximumSize(new Dimension(0, 100));
-		
-		splitPane_1.setRightComponent(scrollPane_1);
-		
+
+		JSplitPane splitPane_1 = new JSplitPane();
+		splitPane_1.setResizeWeight(1.0);
+		splitPane_1.setDividerSize(0);
+		splitPane_1.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPane.setLeftComponent(splitPane_1);
+
+		JPanel paneToplam = new JPanel();
+		//paneToplam.setBorder(new LineBorder(null));
+		paneToplam.setMinimumSize(new Dimension(0, 25));
+		paneToplam.setMaximumSize(new Dimension(0, 25));
+
+		splitPane_1.setRightComponent(paneToplam);
+		paneToplam.setLayout(null);
+
+		lblToplam = new JLabel("0.00");
+		lblToplam.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblToplam.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblToplam.setBounds(695, 5, 150, 15);
+		paneToplam.add(lblToplam);
+
+		lblNewLabel = new JLabel("Kayit Sayisi");
+		lblNewLabel.setBounds(10, 5, 75, 14);
+		paneToplam.add(lblNewLabel);
+
+		lblKayit = new JLabel("0");
+		lblKayit.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblKayit.setBounds(95, 5, 46, 14);
+		paneToplam.add(lblKayit);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportView(table);
+		splitPane_1.setLeftComponent(scrollPane);
+
 		table_1 = new JTable();
 		table_1.setAutoResizeMode( JTable.AUTO_RESIZE_OFF );
 		table_1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -170,168 +182,172 @@ public class TAH_DOKUM extends JInternalFrame {
 	}
 	public static void yenile()
 	{
-		try {
-			
-			//int cins, int tur, String ilktarih, String sontarih, String ilkevr, String sonevr
-			
-		GRID_TEMIZLE.grid_temizle(table);
-		long startTime = System.currentTimeMillis();
-		ResultSet	rs = null;
-		rs = c_Access.tah_listele(FILTRE.comboBox_85.getSelectedIndex(), 
-				FILTRE.comboBox_86.getSelectedIndex(),
-				TARIH_CEVIR.tarih_geri(FILTRE.dateChooser_35),TARIH_CEVIR.tarih_geri(FILTRE.dateChooser_36),
-				FILTRE.textTaheno1.getText(),FILTRE.textTaheno2.getText(),
-				FILTRE.textTahck1.getText(),FILTRE.textTahck2.getText(),FILTRE.comboBox_87.getSelectedItem().toString()	);
-		
-		if (!rs.isBeforeFirst() ) {  
-			return;
-		} 
-		
-		table.setModel(DbUtils.resultSetToTableModel(rs));
-		JTableHeader th = table.getTableHeader();
-		TableColumnModel tcm = th.getColumnModel();
-		TableColumn tc;
-		
-		tc = tcm.getColumn(0);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(70);
-		tc.setMaxWidth(70);
-		
+		try 
+		{
+			GRID_TEMIZLE.grid_temizle(table);
+			long startTime = System.currentTimeMillis();
+			ResultSet	rs = null;
+			rs = c_Access.tah_listele(FILTRE.comboBox_85.getSelectedIndex(), 
+					FILTRE.comboBox_86.getSelectedIndex(),
+					TARIH_CEVIR.tarih_geri(FILTRE.dateChooser_35),TARIH_CEVIR.tarih_geri(FILTRE.dateChooser_36),
+					FILTRE.textTaheno1.getText(),FILTRE.textTaheno2.getText(),
+					FILTRE.textTahck1.getText(),FILTRE.textTahck2.getText(),FILTRE.comboBox_87.getSelectedItem().toString()	);
 
-		tc = tcm.getColumn(1);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setCellRenderer(new TARIH());
-		tc.setMinWidth(70);
-		tc.setMaxWidth(70);
-		
-		tc = tcm.getColumn(2);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(100);
-		tc.setMaxWidth(100);
-		
-		tc = tcm.getColumn(3);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(100);
-		tc.setMaxWidth(100);
-		
-		tc = tcm.getColumn(4);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(70);
-		tc.setMaxWidth(70);
-		
-		tc = tcm.getColumn(5);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(70);
-		tc.setMaxWidth(70);
-		
-		tc = tcm.getColumn(6);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(150);
-		tc.setMaxWidth(150);
-		
-		tc = tcm.getColumn(7);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(70);
-		tc.setMaxWidth(70);
-		
-		tc = tcm.getColumn(8);
-		tc.setHeaderRenderer(new SAGA());
-		tc.setCellRenderer(new TABLO_RENDERER(2,true));
-		tc.setMinWidth(120);
-		tc.setMaxWidth(120);
-		
-		Dimension dd = th.getPreferredSize();
-		dd.height = 30;
-		th.setPreferredSize(dd); 
-		th.repaint();
-		table.setRowHeight(21);
-		
-		long endTime = System.currentTimeMillis();
-		long estimatedTime = endTime - startTime; 
-		double seconds = (double)estimatedTime/1000; 
-		OBS_MAIN.lblNewLabel_9.setText("Son Raporlama Suresi : " + FORMATLAMA.doub_4(seconds) +  " saniye");
+			if (!rs.isBeforeFirst() ) {  
+				lblToplam.setText("0.00");
+				lblKayit.setText("0");
+				return;
+			} 
+			table.setModel(DbUtils.resultSetToTableModel(rs));
+			JTableHeader th = table.getTableHeader();
+			TableColumnModel tcm = th.getColumnModel();
+			TableColumn tc;
 
-		String deger;
-		String[] parts;
-		Font bigFont;
-		deger = GLOBAL.setting_oku("CARI_EKSTRE").toString();
-		deger = deger.substring(1, deger.length()-1);
-		parts = deger.split(",");
-		bigFont = new Font(parts[0], Integer.parseInt(parts[1].trim()), Integer.parseInt(parts[2].trim()));
-		table.setFont(bigFont);
-		table.repaint();
+			tc = tcm.getColumn(0);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(70);
+			tc.setMaxWidth(70);
+
+			tc = tcm.getColumn(1);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setCellRenderer(new TARIH());
+			tc.setMinWidth(70);
+			tc.setMaxWidth(70);
+
+			tc = tcm.getColumn(2);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(100);
+			tc.setMaxWidth(100);
+
+			tc = tcm.getColumn(3);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(100);
+			tc.setMaxWidth(100);
+
+			tc = tcm.getColumn(4);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(70);
+			tc.setMaxWidth(70);
+
+			tc = tcm.getColumn(5);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(70);
+			tc.setMaxWidth(70);
+
+			tc = tcm.getColumn(6);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(183);
+			tc.setMaxWidth(183);
+
+			tc = tcm.getColumn(7);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(60);
+			tc.setMaxWidth(60);
+
+			tc = tcm.getColumn(8);
+			tc.setHeaderRenderer(new SAGA());
+			tc.setCellRenderer(new TABLO_RENDERER(2,true));
+			tc.setMinWidth(120);
+			tc.setMaxWidth(120);
+
+			Dimension dd = th.getPreferredSize();
+			dd.height = 30;
+			th.setPreferredSize(dd); 
+			th.repaint();
+			table.setRowHeight(21);
+
+			long endTime = System.currentTimeMillis();
+			long estimatedTime = endTime - startTime; 
+			double seconds = (double)estimatedTime/1000; 
+			OBS_MAIN.lblNewLabel_9.setText("Son Raporlama Suresi : " + FORMATLAMA.doub_4(seconds) +  " saniye");
+
+			DefaultTableModel mdl = (DefaultTableModel) table.getModel();
+			double toplam = 0 ;
+			for (int i = 0 ; i <= mdl.getRowCount()-1;i++)
+				toplam  +=  (double)   mdl.getValueAt(i , 8);
+			lblToplam.setText(FORMATLAMA.doub_2(toplam));
+			lblKayit.setText(FORMATLAMA.doub_0(mdl.getRowCount()));
+			String deger;
+			String[] parts;
+			Font bigFont;
+			deger = GLOBAL.setting_oku("CARI_EKSTRE").toString();
+			deger = deger.substring(1, deger.length()-1);
+			parts = deger.split(",");
+			bigFont = new Font(parts[0], Integer.parseInt(parts[1].trim()), Integer.parseInt(parts[2].trim()));
+			table.setFont(bigFont);
+			table.repaint();
 		} catch (Exception ex) {
-			 OBS_MAIN.mesaj_goster(5000,Notifications.Type.ERROR,ex.getMessage());
+			OBS_MAIN.mesaj_goster(5000,Notifications.Type.ERROR,ex.getMessage());
 		}
 	}
 	public static void detay_doldur(String evrno,String cins)
 	{
 		try 
 		{
-		int cin = 0 ;
-		if(cins.equals("Tadilat"))
-			cin = 1 ;
-		long startTime = System.currentTimeMillis(); 
-		GRID_TEMIZLE.grid_temizle(table_1);
-		
-		ResultSet	rs = null;
-		rs = c_Access.tah_cek_doldur(evrno,cin	);
-		if (!rs.isBeforeFirst() )
-			return;
-		table_1.setModel(DbUtils.resultSetToTableModel(rs));
-		JTableHeader th = table_1.getTableHeader();
-		TableColumnModel tcm = th.getColumnModel();
-		TableColumn tc;
-		
-		table_1.removeColumn(table_1.getColumnModel().getColumn(0));
-		table_1.removeColumn(table_1.getColumnModel().getColumn(0));
-		
-		tc = tcm.getColumn(0);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(200);
-	
-		tc = tcm.getColumn(1);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(150);
-		
-		tc = tcm.getColumn(2);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(100);
-		tc.setMaxWidth(100);
-		
-		tc = tcm.getColumn(3);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(100);
-		tc.setMaxWidth(100);
-		
-		tc = tcm.getColumn(4);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setMinWidth(150);
-		tc.setMaxWidth(150);
-		
-		tc = tcm.getColumn(5);
-		tc.setHeaderRenderer(new SOLA());
-		tc.setCellRenderer(new TARIH());
-		tc.setMinWidth(70);
-		tc.setMaxWidth(70);
-		
-		tc = tcm.getColumn(6);
-		tc.setHeaderRenderer(new SAGA());
-		tc.setCellRenderer(new TABLO_RENDERER(2,true));
-		tc.setMinWidth(100);
-		tc.setMaxWidth(100);
-		
-		String deger;
-		String[] parts;
-		Font bigFont;
-		deger = GLOBAL.setting_oku("CARI_EKSTRE").toString();
-		deger = deger.substring(1, deger.length()-1);
-		parts = deger.split(",");
-		bigFont = new Font(parts[0], Integer.parseInt(parts[1].trim()), Integer.parseInt(parts[2].trim()));
-		table_1.setFont(bigFont);
-		table_1.repaint();
+			int cin = 0 ;
+			if(cins.equals("Tadilat"))
+				cin = 1 ;
+			long startTime = System.currentTimeMillis(); 
+			GRID_TEMIZLE.grid_temizle(table_1);
+
+			ResultSet	rs = null;
+			rs = c_Access.tah_cek_doldur(evrno,cin);
+			if (!rs.isBeforeFirst())
+				return;
+			table_1.setModel(DbUtils.resultSetToTableModel(rs));
+			JTableHeader th = table_1.getTableHeader();
+			TableColumnModel tcm = th.getColumnModel();
+			TableColumn tc;
+
+			table_1.removeColumn(table_1.getColumnModel().getColumn(0));
+			table_1.removeColumn(table_1.getColumnModel().getColumn(0));
+
+			tc = tcm.getColumn(0);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(200);
+
+			tc = tcm.getColumn(1);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(150);
+
+			tc = tcm.getColumn(2);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(100);
+			tc.setMaxWidth(100);
+
+			tc = tcm.getColumn(3);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(100);
+			tc.setMaxWidth(100);
+
+			tc = tcm.getColumn(4);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setMinWidth(150);
+			tc.setMaxWidth(150);
+
+			tc = tcm.getColumn(5);
+			tc.setHeaderRenderer(new SOLA());
+			tc.setCellRenderer(new TARIH());
+			tc.setMinWidth(70);
+			tc.setMaxWidth(70);
+
+			tc = tcm.getColumn(6);
+			tc.setHeaderRenderer(new SAGA());
+			tc.setCellRenderer(new TABLO_RENDERER(2,true));
+			tc.setMinWidth(100);
+			tc.setMaxWidth(100);
+
+			String deger;
+			String[] parts;
+			Font bigFont;
+			deger = GLOBAL.setting_oku("CARI_EKSTRE").toString();
+			deger = deger.substring(1, deger.length()-1);
+			parts = deger.split(",");
+			bigFont = new Font(parts[0], Integer.parseInt(parts[1].trim()), Integer.parseInt(parts[2].trim()));
+			table_1.setFont(bigFont);
+			table_1.repaint();
 		} catch (Exception ex) {
-			 OBS_MAIN.mesaj_goster(5000,Notifications.Type.ERROR,ex.getMessage());
+			OBS_MAIN.mesaj_goster(5000,Notifications.Type.ERROR,ex.getMessage());
 		}
 	}
 }
